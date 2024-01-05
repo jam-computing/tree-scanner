17f1ef15cc13a88481576a92133cf8724cad57e2
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-01-05 18:02:34 +0100

    Added summary after data input

--- client/client.py
+++ client/client.py
@@ -8,9 +8,12 @@ def main():
 def get_data():
     print('----- Scanner Client -----')
     print('Welcome to the client of the scanner.\nEnsure the server is started.\nPlease fill in the following options. Leaving them blank fills in a default value')
+
     led_count = get_field('How many leds? ', int, 50)
     port = get_field('Which port? ', int, 8765)
     path = get_field('What is the path? ', str, '')
+    
+    print(f'Leds: {led_count}, Port: {port}, Path: {path}')
 
 def get_field(question, parser, default):
     print(question)
