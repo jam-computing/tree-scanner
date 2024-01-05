cae9eaa3fa21e82b5f39a01b0ec028b7634f18f1
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-01-05 15:29:15 +0100

    Continued work on client

--- client/client.py
+++ client/client.py
@@ -13,12 +13,21 @@ class data:
 
 def get_data():
     print('----- Scanner Client -----')
-    print('Welcome to the client of the scanner.\nEnsure the server is started.')
-    led_count = -1
-    while led_count <= 0:
-        led_count = int(input('How many Leds? '))
+    print('Welcome to the client of the scanner.\nEnsure the server is started. Please fill in the following options. Leaving them blank fills a default value')
+
 
     
+
+def get_field(question, parser, default):
+    while True:
+        value = input()
+        if value == '':
+            return default
+        try:
+            return parser(value)
+        except:
+            print('Data invalid, try again')
+   
 
 if __name__ == '__main__':
     main()
