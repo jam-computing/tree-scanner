0825ec30e19b70077626c5fbc010687839759cf9
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-01-07 20:31:20 +0000

    Started work on server

--- server/server.py
+++ server/server.py
@@ -1,59 +1,59 @@
 import asyncio
 from websockets.server import serve
-import board
-import neopixel
+from ipaddress import ip_address
+
+# import board
+# import neopixel
+
 from config_manager import ConfigManager
 from led_manager import LedManager
+
+# Path to config file is within the ConfigManager ctor
+config_manager = ConfigManager("config")
+
+
+def main():
+    print("----- Scanner Server -----")
+    print("Reading in data from the config")
+
+    # Variables from the config
+    ip = config_manager.get_field("ip ?= ?(.+)", ip_address)
+
+    # Start the server
+    asyncio.run(start_server())
+
+
+async def start_server():
+    async with serve(handle_request, path, port):
+        await asyncio.Future()  # run forever
+
 
 async def handle_request(websocket):
     for message in websocket:
         handle_message(websocket, message)
 
+
 def handle_message(websocket, message):
     if message.isdigit():
         index = int(message)
-        print('Wipe update at index : ' + index)
-        wipe_update(int(message))
-        websocket.send('Request processed')
-    elif message == 'setup':
+        print("Wipe update at index : " + index)
+        # wipe_update(int(message))
+        websocket.send("Request processed")
+    elif message == "setup":
         led_count = int(message[:11])
-        print(f'Setting up {led_count} leds')
-        setup_ws281x(led_count)
-        websocket.send('Setup complete')
-    elif message == 'data':
-        print('Data request')
-        websocket.send(f'Path: {path}, Port: {port}, Leds: {led_count}')
-    elif message == 'ping':
-        print('Ping request')
-        websocket.send('pong')
+        print(f"Setting up {led_count} leds")
+        # setup_ws281x(led_count)
+        websocket.send("Setup complete")
+    elif message == "data":
+        print("Data request")
+        # websocket.send(f"Path: {path}, Port: {port}, Leds: {led_count}")
+    elif message == "ping":
+        print("Ping request")
+        websocket.send("pong")
     else:
-        print('Bad request')
-        websocket.send('Bad request')
+        print("Bad request")
+        websocket.send("Bad request")
 
-async def main():
-    async with serve(handle_request, path, port):
-        await asyncio.Future()  # run forever
 
-def get_field(question, parser, default):
-    print(question)
-    while True:
-        value = input()
-        if value == '':
-            return default
-        try:
-            return parser(value)
-        except ValueError:
-            print('Data invalid, try again')
-
-def read_in():
-    print('Reading data from config')
-    config = 
-
-    
-   
-def setup_ws281x():
-    pass
-
-if __name__ == '__main__':
-    print('----- Scanner Server -----')
-    asyncio.run(main())
+if __name__ == "__main__":
+    main()
